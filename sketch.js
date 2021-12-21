b37596b4ecb3f3cc2489729fb8fb09e867a8327b
Sef van Halbeek <81365943+SEFFFFF@users.noreply.github.com>
2021-12-21 15:04:09 +0100

    V0.1.1 Made object size random, added comments

--- sketch.js
+++ sketch.js
@@ -1,18 +1,30 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noLoop();
 }
 
 function draw() {
+  background(255);
+  // These two loops create the grid.
+  // Change the number in i<40 to change the amount of columns.
   for (var i=0; i<40; i++) {
+
+    // Change the number in j<24 to change the amount of rows.
     for (var j=0; j<24; j++){
+
+      // First number in x and y determine the space between objects in the grid.
+      // Second number determines x and y offset from starting point.
       var x= i * 50 + 24;
       var y= j * 50 + 24;
-      var d= 25;
+
+      // Value of d sets the diameter of the drawn objects.
+      var d= random(15, 40);
+      var e= random(15,40);
       
-      fill(0);
+      fill(random(255));
       noStroke();
-      ellipse(x, y, d);
+      ellipse(x, y, d, e);
     }
   };
   
