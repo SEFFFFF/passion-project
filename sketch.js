d6f00b299065765d396c0850c2e2998ac4e075d6
Sef van Halbeek <81365943+SEFFFFF@users.noreply.github.com>
2022-01-11 15:29:44 +0100

    V0.1.1 Added comments

--- sketch.js
+++ sketch.js
@@ -4,24 +4,24 @@ var dia;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
-  
 }
-
 
 function draw() {
   background(0);
+
+  // Sets the beginning of the row in the center of the screen.
   translate(width / 2, height / 2);
+
+  // count dictates the amount of dots based on the mouseX position.
+  // Map translates the mouseX value and reassigns it a value between 0 and 200.
   count = map(mouseX, 0, width / 2, 0, 200);
-  //size = map(mouseY, 0, height, 25, 200);
-  //dia = map(mouseX, 0 , width / 2, 5, 25);
-  //count = 100;
   size = 10;
   for (var i = 0; i < count; i++) {
     rotate((360 / 30) + frameCount * 0.0002);
     noStroke();
     fill(255);
     ellipse(size, size, 10);
+    // size and dia create the offset which make the star shape.
     size += 3;
     dia += 2;
   }
