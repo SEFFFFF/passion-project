c32438288fdbe96ba403dc40c79a64842e6a9954
Sef van Halbeek <81365943+SEFFFFF@users.noreply.github.com>
2022-01-11 15:37:32 +0100

    V0.1 Draws circles based on mouseX

--- sketch.js
+++ sketch.js
@@ -1,28 +1,23 @@
-var count;
-var size;
+var circles;
 var dia;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  background(0);
 }
 
 function draw() {
-  background(0);
-
-  // Sets the beginning of the row in the center of the screen.
-  translate(width / 2, height / 2);
-
-  // count dictates the amount of dots based on the mouseX position.
-  // Map translates the mouseX value and reassigns it a value between 0 and 200.
-  count = map(mouseX, 0, width / 2, 0, 200);
-  size = 10;
-  for (var i = 0; i < count; i++) {
-    rotate((360 / 30) + frameCount * 0.0002);
-    noStroke();
-    fill(255);
-    ellipse(size, size, 10);
-    // size and dia create the offset which make the star shape.
-    size += 3;
-    dia += 2;
+  
+  // Dictates amount of circles based on mouseX value.
+  circles = mouseX / 30 + 1;
+  
+  for (var i = 0; i < 20; i++); {
+    // Sets the min and max diameter of the ellipse.
+    dia = map(i, 0, circles, 25, 250)
+    noFill();
+    stroke(255, 0, 0, 50);
+    ellipse(width / 2, height / 2, dia);
   }
 }
