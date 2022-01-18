4a053be7b40f450a174ea61be58c45aa5f4d9fac
Sef van Halbeek <81365943+SEFFFFF@users.noreply.github.com>
2022-01-18 12:15:06 +0100

    V0.1.1 Added comments

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
-var tileCount = 25;
+var count = 25;
 var col;
 var row;
 var sizeX = 50;
 var sizeY = 28;
 var shape;
- 
+
+// Preload loads SVG shape and puts it in an array.
+// Array can be expanded in the future with more shapes.
 function preload() {
   shape = [];
   shape.push(loadImage('SVG/Asset 1.svg'));
@@ -13,20 +15,23 @@ function preload() {
 function setup() {
   createCanvas(1920, 1080);
   imageMode(CENTER);
+  // Sets the width and height of the tiles in the grid.
   col = width / 25;
   row = height / 15;
 }
  
 function draw() {
+  // Clear gives the canvas a transparant background.
   clear();
  
-  for (var y = 0; y < tileCount; y++) {
-    for (var x = 0; x < tileCount; x++) {
- 
+  for (var y = 0; y < count; y++) {
+    for (var x = 0; x < count; x++) {
+      
+      // Determines position of shapes in the grid.
       var posX = col * x + col / 2;
       var posY = row * y + col / 2;
  
-      // calculate angle between mouse position and actual position of the shape
+      // calculate angle between mouse position and actual position of the shape.
       var angle = atan2(mouseY - posY, mouseX - posX);
  
       push();
